42b5ef0a91df5bc068c52dd429d37e24f5f89a93
Oghenedoro David Agbroko <sirdoro1@gmail.com>
2024-06-24 22:03:32 +0100

    Initial React

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import './App.css'
+
+
+
+function Square({value,onSquareClick}){
+  return (<button className="square" onClick={onSquareClick}>
+              {value}
+          </button>);
+}
+export default function Board() {
+  const [square,setSquare] = useState(Array(9).fill(null));
+// 
+    function handleClick(i){
+     const nextSquares = square.slice();
+      nextSquares[i] = "X";
+      setSquare(nextSquares);
+  }
+
+  return (
+    <>
+      <div className="board-row">
+          <Square value={square[0]} onSquareClick={()=>handleClick(0)} />
+          <Square value={square[1]} onSquareClick={()=>handleClick(1)} />
+          <Square value={square[2]} onSquareClick={()=>handleClick(2)} />
+      </div>
+      <div className="board-row">
+          <Square value={square[3]} onSquareClick={()=>handleClick(3)} />
+          <Square value={square[4]} onSquareClick={()=>handleClick(4)} />
+          <Square value={square[5]} onSquareClick={()=>handleClick(5)} />
+      </div>
+      <div className="board-row">
+          <Square value={square[6]} onSquareClick={()=>handleClick(6)} />
+          <Square value={square[7]} onSquareClick={()=>handleClick(7)} />
+          <Square value={square[8]} onSquareClick={()=>handleClick(8)} />
+      </div>
+    </>
+  );
+}
+
